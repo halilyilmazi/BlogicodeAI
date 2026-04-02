cdb28f16968bb7d948efe23d204500b3692753ac
halilyilmazi <ylmzyzlm@gmail.com>
2026-04-02 08:08:30 +0300

    fix

--- Halil-Yılmaz/backend/index.js
+++ Halil-Yılmaz/backend/index.js
@@ -164,10 +164,9 @@ app.post('/api/chatbot', async (req, res) => {
         const { message } = req.body;
         
         // Yapay zeka anahtarını env dosyasından kurtarıp doğrudan buraya yazdık
-        const apiKey = "AIzaSyAJHD1sZUQwZIyRq-dzi_lyhb1KYFtCFUo".trim();
+        const apiKey = "AIzaSyAJHD1sZUQwZIyRq-dzi_lyhb1KYFtCFUo".trim(); 
         const genAI = new GoogleGenerativeAI(apiKey);
-        
-        const model = genAI.getGenerativeModel({ model: "gemini-pro" }); 
+        const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" }); 
 
         const result = await model.generateContent(message);
         const response = await result.response;
